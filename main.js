7a541dc086ed979e65a5a6b28eab13825d324083
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-06-20 22:53:20 +0200

    Change to arrow functions

--- main.js
+++ main.js
@@ -9,84 +9,125 @@ const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+const nicknameInput = document.getElementById("nickName");
+const rockButton = document.querySelector(".rockButton");
+const cardContainer = document.getElementById("cardContainer");
+const message = document.getElementById("resultMsg");
 
 let currentQuestionIndex = 0;
 let quizz;
 let score = 0;
 
-function hideViews() {
+//------------------------------------------------//
+
+//------------------------------------------------//
+
+const hideViews = () => {
     homeDiv.classList.add("hide");
     resultsDiv.classList.add("hide");
     questionsDiv.classList.add("hide");
-}
+};
 
-function goQuestions() {
+const goQuestions = () => {
     hideViews();
     questionsDiv.classList.remove("hide");
-}
+};
 
-function goHome() {
+const goHome = () => {
     hideViews();
     homeDiv.classList.remove("hide");
-}
+};
 
-function goResults() {
+const generateUserCard = () => {
+    const nickname = localStorage.getItem("nickname");
+    const playerScore = localStorage.getItem(nickname);
+    const cardContent = `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">General score by players</h5>
+          <div class="table-responsive">
+            <table class="table">
+              <tbody>
+                <tr>
+                  <th>Nickname:</th>
+                  <td>${nickname}</td>
+                </tr>
+                <tr>
+                  <th>Score:</th>
+                  <td>${playerScore}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    `;
+
+    cardContainer.innerHTML = cardContent;
+};
+
+const initGameRock = (e) => {
+    e.preventDefault();
+    const user = {
+        nickname: nicknameInput.value,
+        score: score,
+    };
+    goQuestions();
+};
+
+const goResults = () => {
     hideViews();
+    generateUserCard();
     resultsDiv.classList.remove("hide");
-}
+};
 
-function shuffle(array) {
+const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
-}
+};
 
-function getQuestions() {
+const getQuestions = () => {
     axios
         .get("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiplehttps://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple")
         .then((response) => {
             quizz = response.data.results;
-            console.log("Las preguntas retornadas por la API son:", quizz);
             quizz = quizz.map((challenge) => ({
                 question: challenge.question,
                 correctAnswer: challenge.correct_answer,
                 allAnswers: shuffle([...challenge.incorrect_answers, challenge.correct_answer]),
             }));
-            console.log("Las preguntas formateadas son:", quizz);
             startButton.addEventListener("click", () => {
                 resetGame();
-                startGame(quizz);
+                startGame();
             });
         })
         .catch((err) => {
             console.error("Error", err);
         });
-}
+};
 
-function resetState() {
+const resetState = () => {
     nextButton.classList.add("d-none");
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
-}
+};
 
-function setStatusClass() {
+const setStatusClass = () => {
     Array.from(answerButtonsElement.children).forEach((button) => {
         if (button.dataset.correct) {
             button.classList.add("correct");
+            button.classList.add("btn", "btn-success");
         } else {
             button.classList.add("wrong");
+            button.classList.add("btn", "btn-danger");
         }
     });
-}
+};
 
-function selectAnswer(evt) {
-    console.log(this);
-    console.log(evt);
-
-    if (this.dataset.correct === "true") {
+const selectAnswer = function (evt) {
+    if (this.getAttribute("data-correct") === "true") {
         score++;
     }
-    console.log(this.dataset.correct);
-
     if (quizz.length > currentQuestionIndex + 1) {
         nextButton.classList.remove("d-none");
     } else {
@@ -95,47 +136,59 @@ function selectAnswer(evt) {
         showResultMessage();
     }
     setStatusClass();
-}
+    localStorage.setItem(nicknameInput.value, score);
+};
 
-function showQuestion(currentQuestion) {
+const showQuestion = (currentQuestion) => {
     questionElement.innerHTML = currentQuestion.question;
     currentQuestion.allAnswers.forEach((answer) => {
         const button = document.createElement("button");
         button.innerText = answer;
+        button.classList.add("btn", "btn-secondary", "mr-2", "mb-2", "px-4", "py-2");
         if (answer === currentQuestion.correctAnswer) {
             button.dataset.correct = true;
         }
         button.addEventListener("click", selectAnswer);
         answerButtonsElement.appendChild(button);
     });
-}
+};
 
-function setNextQuestion(quizz) {
+const setNextQuestion = () => {
     resetState();
     nextButton.classList.add("d-none");
-    showQuestion(quizz);
-}
+    showQuestion(quizz[currentQuestionIndex]);
+};
 
-function startGame() {
+const startGame = () => {
     startButton.classList.add("d-none");
 
     nextButton.classList.add("d-none");
-    console.log(nextButton.classList);
 
     questionContainerElement.classList.remove("hide");
-    setNextQuestion(quizz[currentQuestionIndex]);
-}
+    setNextQuestion();
+};
 
-function nickName() {}
+const showResultMessage = () => {
+    const card = document.createElement("div");
+    card.classList.add("card", "my-4");
 
-function showResultMessage() {
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
     const resultMessage = document.createElement("p");
-    resultMessage.id = "result-message";
     resultMessage.innerText = `¡Has acertado ${score} preguntas de ${quizz.length}!`;
-    questionContainerElement.appendChild(resultMessage);
-}
+    resultMessage.classList.add("lead");
 
-function resetGame() {
+    cardBody.appendChild(resultMessage);
+    card.appendChild(cardBody);
+
+    message.innerHTML = "";
+    message.appendChild(card);
+
+    localStorage.setItem(nicknameInput.value, score);
+};
+
+const resetGame = () => {
     score = 0;
     currentQuestionIndex = 0;
     questionContainerElement.classList.add("hide");
@@ -149,15 +202,18 @@ function resetGame() {
     if (resultMessage) {
         resultMessage.remove();
     }
-}
+};
 
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
-    setNextQuestion(quizz[currentQuestionIndex]);
+    setNextQuestion();
 });
 
 questionsNav.addEventListener("click", goQuestions);
 homeNav.addEventListener("click", goHome);
 resultsNav.addEventListener("click", goResults);
+rockButton.addEventListener("click", initGameRock);
+
+score = parseInt(localStorage.getItem(nicknameInput.value)) || 0;
 
 getQuestions();
